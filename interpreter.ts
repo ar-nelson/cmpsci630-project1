bfdd98636a1354a16712d34f84b04b9d8b1453e1
Adam R. Nelson <dev@sector91.com>
2014-09-17 09:30:43 -0400

    Added skeleton interpreter file.

---
+++ interpreter.ts
@@ -0,0 +1,22 @@
+﻿
+module Python {
+  class Interpreter {
+
+  }
+
+  interface Printer {
+    /** Prints `text` to the console, without a trailing newline. */
+    print(text: string): void
+    /** Prints a newline character to the console. */
+    printNewline(text: string): void
+  }
+
+  interface Importer {
+    /**
+     * Attempts to import the module `moduleName`, and calls `callback` with a binary ArrayBuffer
+     * if the import succeeded. The first argument of `callback` is an error, which will be
+     * non-null if the import failed.
+     */
+    importModule(moduleName: string, callback: (err: any, bin: ArrayBuffer) => void): void
+  }
+}
