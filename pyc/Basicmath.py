3f07df316fb3d3f2bdaabbfab9855ec692051b47
Arpita Raveendran <arpitaraveendran@gmail.com>
2014-10-03 23:05:03 +0530

    Create Basicmath.py

---
+++ pyc/Basicmath.py
@@ -0,0 +1,32 @@
+a = 21
+b = 10
+c = 0
+c = a + b
+print a, "and ", b
+print "Addition: Value of c is ", c
+c = a - b
+print "Subtraction: Value of c is ", c 
+c = a * b
+print "Product: Value of c is ", c 
+c = a / b
+print "Division Value of c is ", c 
+c = a % b
+print "Mod: Value of c is ", c
+a = 2
+b = 3
+c = a**b 
+print a,"and ",b
+print "Power: Value of c is ", c
+a = 10
+b = 5
+c = a//b 
+print a,"and ",b
+print "Floor division: Value of c is ", c
+ave=(a+b+c)/3
+print "Average is ", ave
+a=10.0
+b=11.1
+c=((a+b)*4)/2
+print a,"and ",b
+print "math with floats ", c
+
