2dfb7357d958e4c0f685d0d2f38e5278ef60d6c8
Arpita Raveendran <arpitaraveendran@gmail.com>
2014-10-03 23:07:52 +0530

    Create ifwithcomparisons.py

---
+++ pyc/ifwithcomparisons.py
@@ -0,0 +1,43 @@
+a = 21
+b = 10
+c = 0
+print a,b
+print "equality check"
+if ( a == b ):
+   print "a is equal to b"
+else:
+   print "a is not equal to b"
+print "inequality check"
+if ( a != b ):
+   print "a is not equal to b"
+else:
+   print "a is equal to b"
+
+if ( a <> b ):
+   print "a is not equal to b"
+else:
+   print "a is equal to b"
+print "lesser than?"
+if ( a < b ):
+   print "a is less than b" 
+else:
+   print "a is not less than b"
+print "greater than?"
+if ( a > b ):
+   print "a is greater than b"
+else:
+   print "a is not greater than b"
+
+a = 5;
+b = 20;
+print a,b
+print "lesser than equal to"
+if ( a <= b ):
+   print "a is either less than or equal to  b"
+else:
+   print "a is neither less than nor equal to  b"
+print "greater than equal to"
+if ( b >= a ):
+   print "b is either greater than  or equal to b"
+else:
+   print "b is neither greater than  nor equal to b"
